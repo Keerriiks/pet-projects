f011ee8afaa7871a16f50866ee0cba78c01b7408
Кирилл Викулин <k.vikulin@inventos.ru>
2024-08-17 00:56:52 +0300

    Правки по EX050

--- EX050/lib/fake_student.rb
+++ EX050/lib/fake_student.rb
@@ -1,37 +1,40 @@
 class FakeStudent
-  ALLOWED_FIRST_NAMES = ["Alice", "Bob", "Carl"]
+  DEFAULT_FIRST_NAMES = ["Alice", "Bob", "Carl"]
   DEFAULT_LAST_NAMES = ["Roberts", "Smith", "Thompson"]
 
-  # NOTE: Возраст студента.
-  # @return [Integer | nil]
-  attr_accessor :age
+  attr_writer :age
 
   attr_writer :last_name
 
-  # @return [Hash]
   def initialize(attrs={})
     attrs.each { |k, v| public_send("#{k}=", v) }
   end
 
-  # NOTE: Имя студента.
+  # Возраст студента.
+  # @return [Integer | nil]
+  def age
+    @age.to_i if defined?(@age) && !@age.to_s[/^\d+$/].nil?
+  end
+
+  # Имя студента.
   # @return [String]
   def first_name
     return @first_name if defined? @first_name
-    @first_name = ALLOWED_FIRST_NAMES.sample
+    @first_name = DEFAULT_FIRST_NAMES.sample
   end
 
-  # NOTE: Фамилия студента.
+  # Фамилия студента.
   # @return [String]
   def last_name
-    return @last_name if defined? @last_name
+    return @last_name.to_s if defined? @last_name
     @last_name = DEFAULT_LAST_NAMES.sample
   end
 
-  # NOTE: Форматированная информация о студенте.
+  # Форматированная информация о студенте.
   # @return [String]
   def personal_info
     return @personal_info if defined? @personal_info
-    @personal_info = "#{first_name} #{last_name}#{age.nil? ? '' : 'age , ' + age.to_s}"
+    @personal_info = "#{first_name} #{last_name}#{age ? ', ' + age.to_s : ''}"
   end
 end
 
