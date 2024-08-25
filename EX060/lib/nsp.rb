385b20a93ae0232fb8973e9b0189d4e905b699c6
Кирилл Викулин <k.vikulin@inventos.ru>
2024-08-25 23:34:56 +0300

    Сделал help и вызовы nsp

--- EX060/lib/nsp.rb
+++ EX060/lib/nsp.rb
@@ -1,1 +1,36 @@
-# ...
+module Nsp
+
+  # @return [nil]
+  def nsp(args={})
+
+    # ...
+
+    help
+  end
+
+  # @return [nil]
+  def nsp!(args={})
+
+    # ...
+
+    help
+  end
+
+  private
+
+  # @return [String]
+  def help
+    puts "\033[1;33;40mUSAGE: <obj>.nsp(<meth>, [...args])\033[0m
+
+\033[1;33;40mEXAMPLES:\033[0m
+
+* Class.nsp :grep, /inst/
+* \"\".nsp! :grep, //
+* {}.nsp! :grep, //
+    "
+  end
+end
+
+class Object
+  include Nsp
+end
