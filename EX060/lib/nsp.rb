bc2ce6d66250ec043be5cea0b9ff6196c79c2182
Кирилл Викулин <k.vikulin@inventos.ru>
2024-09-03 22:57:18 +0300

    disc: на обсуждение help_with_arguments

--- EX060/lib/nsp.rb
+++ EX060/lib/nsp.rb
@@ -2,16 +2,14 @@ module Nsp
 
   # @return [nil]
   def nsp(*args)
-    return self_help if args.size == 0
-
-    puts methods_info(optional_object_methods(args[0], args[1]))
+    return help_with_arguments(args)
+    distributor(args, true)
   end
 
   # @return [nil]
   def nsp!(*args)
-    return self_help if args.size == 0
-
-    puts methods_info(optional_object_methods(args[0], args[1], false))
+    return help_with_arguments(args)
+    distributor(args, false)
   end
 
   private
@@ -19,6 +17,18 @@ module Nsp
   # @return [Array]
   def cognition_methods
     self.methods.select { |m| m.to_s =~ /methods$/ }
+  end
+
+  # @return [nil]
+  def distributor(args, include_super)
+    return help_with_arguments(args)
+    puts methods_info(optional_object_methods(args[1], include_super)) if args[0] == :grep
+  end
+
+  # @return [nil]
+  def help_with_arguments(args)
+    puts self_help if args.size == 0
+    puts "\033[0;31;40m#<ArgumentError: wrong number of arguments (given #{args.size}, expected 2)>\033[0m" if args.size > 2
   end
 
   # @return [String]
@@ -42,19 +52,17 @@ module Nsp
   end
 
   # @return [Hash]
-  def optional_object_methods(meth, args, include_super = true)
-    if meth == :grep
+  def optional_object_methods(args, include_super)
       hash = {}
       object_methods(include_super).each do |key, value|
-        hash[key] = !args.nil? ? value.select { |m| args.match?(m.to_s) } : "\033[0;31;40m#<ArgumentError: wrong number of arguments (given 1, expected 2)>\033"
+        hash[key] = !args.nil? ? value.select { |m| args.match?(m.to_s) } : "\033[0;31;40m#<ArgumentError: wrong number of arguments (given 1, expected 2)>\033[0m"
       end
       hash
-    end
   end
 
   # @return [String]
   def self_help
-    puts "\033[1;33;40mUSAGE: <obj>.nsp(<meth>, [...args])\033[0m
+  "\033[1;33;40mUSAGE: <obj>.nsp(<meth>, [...args])\033[0m
 
 \033[1;33;40mEXAMPLES:\033[0m
 
